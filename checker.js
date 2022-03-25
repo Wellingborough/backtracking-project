021f66e996b72230c421cfab7c829ef399ce160c
xiphiasxiphian <xiphias.xiphian@gmail.com>
2022-03-25 10:46:31 +0000

    commit

---
+++ checker.js
@@ -0,0 +1,33 @@
+var board = [8, 2, 5, 3, 1, 7, 4, 6]
+
+function columnChecker(board)
+{
+    if (board.length == [...new Set(board)].length) {return true;}
+    else {return false;}
+}
+
+function dcheck(board)
+{
+    for (var i = 0; i < board.length; i++)
+    {
+        var current = board[i];
+        for (var j = i + 1; j < board.length; j++)
+        {
+            var checkers = board[j];
+            if (current == (checkers + (j - i))) {return false;}
+            else if (current == (checkers - (j - i))) {return false;}
+        }
+    }
+    return true;
+}
+
+function grandChecker(board)
+{
+    if (columnChecker(board))
+    {
+        if (dcheck(board)) {return true;}
+    }
+    return false
+}
+
+console.log(grandChecker(board))
--- sketch.js
+++ sketch.js
@@ -1,11 +1,14 @@
 
-const SCREEN_WIDTH = 400;
-const SCREEN_HEIGHT = 400;
+const SCREEN_WIDTH = 600;
+const SCREEN_HEIGHT = 600;
+
+const CHESS_WIDTH = 400;
+const CHESS_HEIGHT = 400;
 
 const n = 8;
 
-const squareWidth = SCREEN_WIDTH / n;
-const squareHeight = SCREEN_HEIGHT / n;
+const squareWidth = CHESS_WIDTH / n;
+const squareHeight = CHESS_HEIGHT / n;
 
 function drawBoard()
 {
@@ -42,64 +45,129 @@ function drawBoard()
     }
 }
 
-function moveQueen(index, newx, newy, animation)
-{
-    if (!animation)
-    {
-        queens[index].x = newx;
-        queens[index].y = newy;
-    }
-}
-
 var queens;
 
 var previousTime;
+var dt;
+
+var moves = []
+
 function setup()
 {
     var canvas = createCanvas(SCREEN_WIDTH, SCREEN_HEIGHT);
     canvas.center();
 
-    queens = [
-        {black: loadImage("./resources/black_queen.png"), white: loadImage("./resources/white_queen.png"), x: 0, y: 0},
-        {black: loadImage("./resources/black_queen.png"), white: loadImage("./resources/white_queen.png"), x: 1, y: 0},
-        {black: loadImage("./resources/black_queen.png"), white: loadImage("./resources/white_queen.png"), x: 2, y: 0},
-        {black: loadImage("./resources/black_queen.png"), white: loadImage("./resources/white_queen.png"), x: 3, y: 0},
-        {black: loadImage("./resources/black_queen.png"), white: loadImage("./resources/white_queen.png"), x: 4, y: 0},
-        {black: loadImage("./resources/black_queen.png"), white: loadImage("./resources/white_queen.png"), x: 5, y: 0},
-        {black: loadImage("./resources/black_queen.png"), white: loadImage("./resources/white_queen.png"), x: 6, y: 0},
-        {black: loadImage("./resources/black_queen.png"), white: loadImage("./resources/white_queen.png"), x: 7, y: 0}
-    ]
+    var movePieceInterval = setInterval(movePiece, 1000)
 
-    //moveQueen(0, 0, 3, false)
+    queens = [
+        {index: 0, black: loadImage("./resources/black_queen.png"), white: loadImage("./resources/white_queen.png"), x: 0, y: 0, active: false},
+        {index: 1, black: loadImage("./resources/black_queen.png"), white: loadImage("./resources/white_queen.png"), x: 1, y: 0, active: false},
+        {index: 2, black: loadImage("./resources/black_queen.png"), white: loadImage("./resources/white_queen.png"), x: 2, y: 0, active: false},
+        {index: 3, black: loadImage("./resources/black_queen.png"), white: loadImage("./resources/white_queen.png"), x: 3, y: 0, active: false},
+        {index: 4, black: loadImage("./resources/black_queen.png"), white: loadImage("./resources/white_queen.png"), x: 4, y: 0, active: false},
+        {index: 5, black: loadImage("./resources/black_queen.png"), white: loadImage("./resources/white_queen.png"), x: 5, y: 0, active: false},
+        {index: 6, black: loadImage("./resources/black_queen.png"), white: loadImage("./resources/white_queen.png"), x: 6, y: 0, active: false},
+        {index: 7, black: loadImage("./resources/black_queen.png"), white: loadImage("./resources/white_queen.png"), x: 7, y: 0, active: false}
+    ]
 
     const d = new Date();
     previousTime = d.getTime();
+
+    moves.push({index: 0, dx: 0, dy: 0, toggle: true})
+    moves.push({index: 0, dx: 5, dy: 3, toggle: false})
+    moves.push({index: 0, dx: 0, dy: 0, toggle: true})
+
+    dt = 0
 }
+
 function draw()
 {
+    clear()
     const d = new Date();
     const currentTime = d.getTime();
 
-    const dt = currentTime - previousTime;
+    dt = currentTime - previousTime;
     previousTime = currentTime;
 
-
     drawBoard()
-
+    var number = 0;
     for (queen of queens)
     {
-        queen_image = null;
-
-        if ((queen.x % 2 == 1 || queen.y % 2 == 1))
+        number++;
+        if (queen.active)
         {
-            queen_image = queen.white;
+            queen_image = null;
+
+            if (queen.y % 2 == 1)
+            {
+                if (queen.x % 2 == 1)
+                {
+                    queen_image = queen.black;
+                }
+                else
+                {
+                    queen_image = queen.white;
+                }
+            }
+            else if (queen.y % 2 == 0)
+            {
+                if (queen.x % 2 == 1)
+                {
+                    queen_image = queen.white;
+                }
+                else
+                {
+                    queen_image = queen.black;
+                }
+            }
+            else
+            {
+                console.log("something has gone terribly wrong")
+            }
+            image(queen_image, queen.x * squareWidth, queen.y * squareHeight, squareWidth, squareHeight);
         }
         else
         {
-            queen_image = queen.black;
-        }
-        
+            image(queen.black, ((n - queen.index) * squareWidth - squareWidth), squareHeight * 9, squareWidth, squareHeight);
+        } 
+    }
+    console.log(number)
+}
 
-        image(queen_image, queen.x * squareWidth, queen.y * squareHeight, squareWidth, squareHeight);
+
+function movePiece()
+{
+    if (moves.length > 0)
+    {
+        var move = moves[0]
+
+        if (move.toggle)
+        {
+            if (queens[move.index].active)
+            {
+                queens[move.index].active = false;
+            }
+            else
+            {
+                queens[move.index].active = true;
+            }
+        }
+
+        if (move.dx != 0)
+        {
+            const dx = move.dx;
+            queens[move.index].x += Math.round(dx / Math.abs(dx))
+            move.dx -= Math.round(dx / Math.abs(dx))
+        }
+        else if (move.dy != 0)
+        {
+            const dy = move.dy;
+            queens[move.index].y += Math.round(dy / Math.abs(dy))
+            move.dy -= Math.round(dy / Math.abs(dy))
+        }
+        else
+        {
+            moves.shift()
+        }
     }
 }
