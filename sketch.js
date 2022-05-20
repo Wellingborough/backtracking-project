83a816cba810f06f5cdf8ae7a3c057b7719ebf74
Mark Gamble <58432939+Wellingborough@users.noreply.github.com>
2022-05-20 16:37:30 +0100

    Tracing #3

--- sketch.js
+++ sketch.js
@@ -249,6 +249,7 @@ function calculateMoves()
     var package = run()
     var boardHistory = package[0]
     var solutionNumbers = package[1]
+    console.log(boardHistory)
 
     for (tmpBoard of boardHistory)
     {
